4c4c18c489dc83cc86ce3c4e06aec3c39ae6335c
Leonardo Rodrigues de Freitas <leonardodefreitas1995@yahoo.com.br>
2020-09-20 08:36:59 -0300

    fiz umas 3 atividades da lista

--- aulasProgramacao1/lista0.py
+++ aulasProgramacao1/lista0.py
@@ -59,3 +59,68 @@ while n > 0:
 print("%i:%i:%i" %(hours, minutes, seconds))
 
  """
+
+""" num1 = input("").split(" ")
+num2 = input("").split(" ")
+x1 = float(num1[0])
+y1 = float(num1[1])
+x2 = float(num2[0])
+y2 = float(num2[1])
+
+distance = (((x2 - x1)**2) + ((y2 - y1)**2)) ** (1/2)
+
+print("%.4f" %(distance)) """
+
+""" distance = int(input(""))
+
+minutes = distance * 2
+
+print("%i minutos" %(minutes)) """
+
+""" n = int(input())
+
+ano = 0
+meses = 0
+dias = 0
+
+while n > 0:
+  dias += 1
+
+  if dias == 30:
+    meses += 1
+    dias = 0
+    if meses == 12:
+      ano += 1
+      meses = 0
+      dias = -5
+
+  n -= 1
+
+print("%i ano(s)\n%i mes(es)\n%i dia(s)" %(ano, meses, dias)) """
+
+salary = float(input(""))
+percentual = 0
+earned = 0
+
+if salary >= 0 and salary <= 400:
+  percentual = 0.15
+  earned = salary * percentual
+elif salary >= 400.01 and salary <= 800:
+  percentual = 0.12
+  earned = salary * percentual
+elif salary >= 800.01 and salary <= 1200:
+  percentual = 0.10
+  earned = salary * percentual
+elif salary >= 1200.01 and salary <= 2000:
+  percentual = 0.07
+  earned = salary * percentual
+elif salary > 2000:
+  percentual = 0.04
+  earned = salary * percentual
+
+new_salary = salary + earned
+percentual = int(percentual * 100)
+
+print("Novo salario: %.2f" %(new_salary))
+print("Reajuste ganho: %.2f" %(earned))
+print("Em percentual: %i %%" %(percentual))
